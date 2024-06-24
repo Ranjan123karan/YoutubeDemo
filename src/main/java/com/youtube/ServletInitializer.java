df1e6a956ba0a67992b9288b1e7e25566da6080a
Ranjan <ranjanchaudhary@gmail.com>
2024-06-24 11:21:09 +0530

    servlet main commit

--- src/main/java/com/youtube/ServletInitializer.java
+++ src/main/java/com/youtube/ServletInitializer.java
@@ -1,9 +1,14 @@
 package com.youtube;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
+	
+	 public static void main(String[] args) {
+	        SpringApplication.run(YoububeApplication.class, args);
+	    }
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
